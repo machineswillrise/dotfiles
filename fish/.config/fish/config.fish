63973e8a89569a4a0966c137991a8511510babd2
MachinesWillRise <machineswillrise@protonmail.com>
2026-05-20 13:03:51 -0400

    Add Fish config

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,35 @@
+# Load CachyOS config
+source /usr/share/cachyos-fish-config/cachyos-config.fish
+
+# Add JBang to environment
+alias j!=jbang
+export PATH="$HOME/.jbang/bin:$PATH"
+
+# Editors and man paging
+export EDITOR=emacs
+export SUDO_EDITOR=emacs
+export PAGER=most
+
+# Use nicer looking alternatives to common utilities
+alias grep="rg"
+alias cat="bat"
+alias ls="exa"
+
+# Commands I frequently type wrong
+alias sdkman="sdk"
+
+# Sourcehut SSH agent
+eval "$(ssh-agent -c)" > /dev/null
+
+# Start screen
+if [ -z "$TTY" ]
+	screen -xRR terminal
+end
+
+# Alias to kill all screens
+alias kscreen="pkill screen"
+
+function fish_greeting
+	# Do nothing
+end
+
