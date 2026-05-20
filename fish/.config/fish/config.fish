9206aaf71c5d07f8cb9291e40e35bcb8ea3131f3
MachinesWillRise <machineswillrise@protonmail.com>
2026-05-20 13:03:51 -0400

    Add hotspot bypass

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -42,6 +42,14 @@ end
 
 # Load ESP-IDF SDK
 alias get_idf=". $HOME/esp/esp-idf/export.fish"
+
+# Bypass hotspot throttling
+function bypass_hotspot
+	sudo iptables -t mangle -A PREROUTING -i wlan0 -j TTL --ttl-inc 1
+	sudo iptables -t mangle -I POSTROUTING -o wlan0 -j TTL --ttl-inc 1
+	sudo ip6tables -t mangle -A PREROUTING ! -p icmpv6 -i wlan0 -j HL --hl-inc 1
+	sudo ip6tables -t mangle -I POSTROUTING ! -p icmpv6 -o wlan0 -j HL --hl-inc 1
+end
 # ----------------
 
 function fish_greeting
